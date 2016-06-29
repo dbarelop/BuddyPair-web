fcae9d958c81591dfb433a4448a7a802ae9a263f
dbarelop <dbarelop@gmail.com>
2016-06-29 20:14:18 +0200

    Fixed bug with spinner, added also to profile pages

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -42,29 +42,35 @@ angular.module('myApp.controllers', []).
   }).
   controller('ErasmusCtrl', function($scope, $route, $routeParams, $http) {
     $scope.$route = $route;
-    if ($routeParams.id)
-      $http.get('/api/erasmus/' + $routeParams.id).then(function(data) {
+    if ($routeParams.id) {
+      $scope.erasmus = null;
+      $http.get('/api/erasmus/' + $routeParams.id).then(function (data) {
         $scope.erasmus = data.data[0];
-        $http.get('/api/erasmus/' + $routeParams.id + '/assignedPeer').then(function(data) {
+        $http.get('/api/erasmus/' + $routeParams.id + '/assignedPeer').then(function (data) {
           $scope.assignedPeer = data.data[0];
         });
       });
-    else
-      $http.get('/api/erasmusList').then(function(data) {
+    } else {
+      $scope.erasmusList = null;
+      $http.get('/api/erasmusList').then(function (data) {
         $scope.erasmusList = data.data;
       });
+    }
   }).
   controller('PeerCtrl', function($scope, $route, $routeParams, $http) {
     $scope.$route = $route;
-    if ($routeParams.id)
-      $http.get('/api/peer/' + $routeParams.id).then(function(data) {
+    if ($routeParams.id) {
+      $scope.peer = null;
+      $http.get('/api/peer/' + $routeParams.id).then(function (data) {
         $scope.peer = data.data[0];
-        $http.get('/api/peer/' + $routeParams.id + '/assignedErasmus').then(function(data) {
+        $http.get('/api/peer/' + $routeParams.id + '/assignedErasmus').then(function (data) {
           $scope.assignedErasmus = data.data;
         });
       });
-    else
-      $http.get('/api/peerList').then(function(data) {
+    } else {
+      $scope.peerList = null;
+      $http.get('/api/peerList').then(function (data) {
         $scope.peerList = data.data;
       });
+    }
   });
