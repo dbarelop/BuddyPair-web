9873ffebd515e6e9fa568bc7a9e2110ad8f911e2
dbarelop <dbarelop@gmail.com>
2017-01-23 12:47:56 +0100

    Split ErasmusCtrl and PeerCtrl into ErasmusListCtrl and PeerListCtrl, respectively

--- public/js/controller/erasmusCtrl.js
+++ public/js/controller/erasmusCtrl.js
@@ -49,24 +49,4 @@ angular.module('BuddyPairApp.controllers')
         });
       }
     };
-  }])
-  .controller('ErasmusListCtrl', ['$scope', '$route', '$routeParams', 'ErasmusService', function($scope, $route, $routeParams, ErasmusService) {
-    $scope.$route = $route;
-    $scope.filters = {
-      withPeer: 'all'
-    };
-
-    $scope.filter_erasmus = function(e) {
-      var filters = $scope.filters;
-      var nameFilter = !filters.name || (e.name + " " + e.surname).toLowerCase().indexOf(filters.name.toLowerCase()) > -1;
-      var assignedPeerFilter = !filters.withPeer || (filters.withPeer == 'all') ||
-        (filters.withPeer == 'y' && e.has_peer) || (filters.withPeer == 'n' && !e.has_peer);
-      return nameFilter && assignedPeerFilter;
-    };
-
-    ErasmusService.getList().then(function(data) {
-      $scope.erasmusList = data;
-    }, function(err) {
-      $scope.error = err.message.code;
-    });
   }]);
--- public/js/controller/peerCtrl.js
+++ public/js/controller/peerCtrl.js
@@ -93,35 +93,4 @@ angular.module('BuddyPairApp.controllers')
         });
       }
     };
-  }])
-  .controller('PeerListCtrl', ['$scope', '$route', '$routeParams', 'PeerService', function($scope, $route, $routeParams, PeerService) {
-    $scope.$route = $route;
-    $scope.filters = {
-      numErasmus: {
-        zero: true,
-        one: true,
-        two: true,
-        three: true
-      }
-    };
-
-    $scope.filter_peers = function(p) {
-      var filters = $scope.filters;
-      var nameFilter = !filters.name || (p.name + " " + p.surname).toLowerCase().indexOf(filters.name.toLowerCase()) > -1;
-      var assignedErasmusFilter = false;
-      switch (p.num_erasmus) {
-        case 0: assignedErasmusFilter = filters.numErasmus.zero; break;
-        case 1: assignedErasmusFilter = filters.numErasmus.one; break;
-        case 2: assignedErasmusFilter = filters.numErasmus.two; break;
-        case 3: assignedErasmusFilter = filters.numErasmus.three; break;
-        default: assignedErasmusFilter = filters.numErasmus.three;
-      }
-      return nameFilter && assignedErasmusFilter;
-    };
-    $scope.peerList = null;
-    PeerService.getList().then(function (peerList) {
-      $scope.peerList = peerList;
-    }, function (err) {
-      $scope.error = err.message.code;
-    });
   }]);
