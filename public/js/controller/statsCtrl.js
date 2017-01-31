a9bae2b28323cdc6e96756682a25b24e11141b04
dbarelop <dbarelop@gmail.com>
2017-01-31 18:19:19 +0100

    Added registered Erasmus / peers over time charts

--- public/js/controller/statsCtrl.js
+++ public/js/controller/statsCtrl.js
@@ -3,6 +3,7 @@
 angular.module('BuddyPairApp.controllers')
   .controller('StatsCtrl', ['$scope', '$route', '$http', 'ErasmusService', 'PeerService', function($scope, $route, $http, ErasmusService, PeerService) {
     $scope.$route = $route;
+    
     $scope.numRegistered = {
       data: [[0], [0]],
       labels: ['2016'],
@@ -21,14 +22,39 @@ angular.module('BuddyPairApp.controllers')
         }
       }
     };
+    
     $scope.genderErasmus = {
       data: [0, 0],
       labels: ['Male', 'Female']
     };
+    
     $scope.genderPeers = {
       data: [0, 0],
       labels: ['Male', 'Female']
     };
+
+    $scope.registeredErasmus = {
+      data: [[]],
+      options: {
+        scales: {
+          xAxes: [{
+            type: 'time'
+          }]
+        }
+      }
+    };
+
+    $scope.registeredPeers = {
+      data: [[]],
+      options: {
+        scales: {
+          xAxes: [{
+            type: 'time'
+          }]
+        }
+      }
+    };
+    
     ErasmusService.getCount().then(function (count) {
       $scope.male_erasmus = count.male_erasmus;
       $scope.female_erasmus = count.female_erasmus;
@@ -39,6 +65,7 @@ angular.module('BuddyPairApp.controllers')
     }, function (err) {
       $scope.error = err.message.code;
     });
+    
     PeerService.getCount().then(function (count) {
       $scope.male_peers = count.male_peers;
       $scope.female_peers = count.female_peers;
@@ -49,4 +76,25 @@ angular.module('BuddyPairApp.controllers')
     }, function (err) {
       $scope.error = err.message.code;
     });
+    
+    ErasmusService.getList().then(function(erasmus) {
+      $scope.registeredErasmus.data[0] = [];
+      var n = 0;
+      erasmus.forEach(function(e) {
+        $scope.registeredErasmus.data[0].push({y: ++n, x: new Date(e.register_date)})
+      });
+    }, function(err) {
+      $scope.error = err.message.code;
+    });
+    
+    PeerService.getList().then(function(peers) {
+      $scope.registeredPeers.data[0] = [];
+      var n = 0;
+      peers.forEach(function(p) {
+        $scope.registeredPeers.data[0].push({y: ++n, x: new Date(p.register_date)})
+      });
+    }, function(err) {
+      $scope.error = err.message.code;
+    });
+    
   }]);
