44e0856a34085afb4307ebf3e668fd5d4e26c23d
dbarelop <dbarelop@gmail.com>
2017-07-04 00:21:46 +0200

    Added gender and nationality (incomplete) filters for the Erasmus list

--- public/js/controller/erasmusListCtrl.js
+++ public/js/controller/erasmusListCtrl.js
@@ -5,15 +5,18 @@ angular.module('BuddyPairApp.controllers')
     $scope.$route = $route;
     $scope.$location = $location;
     $scope.filters = {
+      gender: 'all',
+      nationality: '',
       withPeer: 'all'
     };
 
     $scope.filter_erasmus = function(e) {
       var filters = $scope.filters;
       var nameFilter = !filters.name || (e.name + " " + e.surname).toLowerCase().indexOf(filters.name.toLowerCase()) > -1;
-      var assignedPeerFilter = !filters.withPeer || (filters.withPeer === 'all') ||
-        (filters.withPeer === 'y' && e.has_peer) || (filters.withPeer === 'n' && !e.has_peer);
-      return nameFilter && assignedPeerFilter;
+      var genderFilter = !filters.gender || (filters.gender === 'all') || (filters.gender === 'm' && e.gender) || (filters.gender === 'f' && !e.gender);
+      var nationalityFilter = !filters.nationality || (filters.nationality === '') || (filters.nationality.country_code === e.nationality);
+      var assignedPeerFilter = !filters.withPeer || (filters.withPeer === 'all') || (filters.withPeer === 'y' && e.has_peer) || (filters.withPeer === 'n' && !e.has_peer);
+      return nameFilter && genderFilter && nationalityFilter && assignedPeerFilter;
     };
     
     $scope.match_students = function(semester_id) {
